fd557813c29afa29fbeadb9078946b735ba2cee0
MOMONBE580\USER <momo29.yang@gmail.com>
2023-06-18 18:12:13 +0800

    second commit

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("I just added this in visual studio");
+Console.WriteLine("I just added this in visual studio。");
